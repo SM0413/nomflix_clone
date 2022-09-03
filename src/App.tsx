16d5b3bd6188e3c7cba79452ad735dc91fd1248e
SM0413 <kbjtmdals@naver.com>
2022-09-03 15:25:56 +0900

    Header part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Routes/Tv.tsx
@@ -0,0 +1,4 @@
+function Tv() {
+  return <h1>Hi</h1>;
+}
+export default Tv;
